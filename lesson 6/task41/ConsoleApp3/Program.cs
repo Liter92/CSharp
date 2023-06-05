d84a04f4b18b4a3015c440b3db23204fa2f0c335
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-06-05 19:45:09 +0300

    Add files via upload

---
+++ lesson 6/task41/ConsoleApp3/Program.cs
@@ -0,0 +1,79 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите числа через запятую: ");
+        int[] numbers = StringToNum(Console.ReadLine());
+        PrintArray(numbers);
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > 0)
+            {
+                sum++;
+            }
+        }
+
+        int[] StringToNum(string input)
+        {
+            int count = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ',')
+                {
+                    count++;
+                }
+            }
+
+            int[] numbers = new int[count];
+            int index = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string temp = "";
+                try
+                {
+
+                    while (input[i] != ',')
+                    {
+                        if (i == input.Length - 1)
+                        {
+
+                            temp += input[i].ToString();
+                            break;
+                        }
+                        else
+                        {
+                            temp += input[i].ToString();
+                            i++;
+                        }
+                    }
+                    numbers[index] = Convert.ToInt32(temp);
+                    index++;
+                    
+
+                }
+                catch (FormatException)
+                {                   
+                    Console.WriteLine("Проверьте вводимые данные!");
+                    break;
+                }
+              
+            }
+            return numbers;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"кол-во чисел >0 = {sum}");
+        void PrintArray(int[] array)
+        {
+            Console.Write("| ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+            Console.Write("|");
+
+        }
+    }
+}
