1f4adfa18c06ef2779d5a76bac08d7c92a031bcc
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-06-05 19:24:24 +0300

    Add files via upload

---
+++ lesson 5/task 38/task 38/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("\n\nЗадание 38 №\nВведите размер массива");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 2)
+{
+    Console.WriteLine("Ошибка, Введите положительные значения");
+}
+else
+{
+      int[] array = new int[N];
+    int min = int.MaxValue;
+    int max = int.MinValue;
+    Random random = new Random();
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        ArayRandom(array, a);
+        a = random.Next(-100, 100);
+        array[i] = a;
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    dfd(array);
+    Console.WriteLine($"Минимальное значение: {min}");
+    Console.WriteLine($"Максимальное значение: {max}");
+    int difference = max - min;
+    Console.WriteLine($"Разница между самым большим и самым маленьким значением: {difference}");
+
+    Console.ReadLine();
+
+
+    static bool ArayRandom(int[] array, int a)
+    {
+        return false;
+    }
+
+    void dfd(int[] array)
+    {
+        Console.Write("|");
+        for (int i = 0; i < array.Length; i++)
+
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.Write("|\n");
+
+    }
+}
---
+++ lesson 5/task34/task34/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("\n\nЗадание 36 №\nВведите размер массива");
+int s1 = Convert.ToInt32(Console.ReadLine());
+if (s1 < 1)
+{
+    Console.WriteLine("Ошибка, Введите положительные значения");
+}
+else
+{
+
+    int[] arr = new int[s1];
+    zapolnenie(arr);
+    Console.WriteLine("Вот наш массив: ");
+    vivod(arr);
+    int sum = 0;
+
+    for (int z = 0; z < arr.Length; z += 2)
+        sum = sum + arr[z];
+
+    Console.WriteLine($"всего {arr.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+    void zapolnenie(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(1, 999);
+        }
+    }
+    void vivod(int[] arr)
+    {
+        Console.Write("|");
+        for (int i = 0; i < arr.Length; i++)
+
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.Write("|\n");
+
+    }
+}
