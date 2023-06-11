d89b6ba62ee5424716b242645b73d490afba214a
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-06-11 15:56:35 +0300

    Add files via upload

---
+++ lesson 7/zadanie 47/zadanie 47/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите размерность m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0 && m > 0)
+{
+    double[,] randomArray = new double[m, n];
+    void ZapolnenieRand(int m, int n)
+    {
+        int i, V;
+        Random rand = new Random();
+        for (i = 0; i < m; i++)
+        {
+            Console.WriteLine();
+            for (V = 0; V < n; V++)
+            {
+                randomArray[i, V] = rand.NextDouble() * 100;
+                Console.Write("{0,6:F1}", randomArray[i, V]);
+            }
+        }
+    }
+    ZapolnenieRand(m, n);
+}
+else Console.WriteLine("Введите корректные данные");
---
+++ lesson 7/zadanie 52/zadanie 52/Program.cs
@@ -0,0 +1,36 @@
+﻿Random random = new Random();
+int[,] arr = new int[random.Next(4, 8), random.Next(4, 8)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    Console.Write("|");
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 100);
+        Console.Write(arr[i, j] + " ");
+        
+
+    }
+    Console.Write("|");
+    Console.WriteLine();
+}
+Console.WriteLine("\n");
+Console.WriteLine("кол-во элементов в столбце: "+arr.GetLength(0));
+Console.WriteLine("\nСреднее значение в каждом столбце:");
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+ 
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+
+    Console.Write($"{sum / arr.GetLength(0)} ");
+  
+ 
+}
+Console.ReadKey();
+
+
+
