cb901d6c1efd500a96368116fe94cd95260a5a35
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-06-15 19:22:09 +0300

    Add files via upload

---
+++ lesoon 8 part 2/zadanie 58/zadanie 58/Program.cs
@@ -0,0 +1,83 @@
+﻿
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+if (m!=n & n!=p & p!=n & n!=p)
+{
+    Console.WriteLine("\nВведите корректные даные");
+}
+else
+{
+
+    var R = new Random();
+    int range = R.Next(1, 1000);
+
+
+    int[,] firstMartrix = new int[m, n];
+    CreateArray(firstMartrix);
+    Console.WriteLine($"Первая матрица:");
+    writearr(firstMartrix);
+
+    int[,] secomdMartrix = new int[n, p];
+    CreateArray(secomdMartrix);
+    Console.WriteLine($"Вторая матрица:");
+    writearr(secomdMartrix);
+
+    int[,] resultMatrix = new int[m, p];
+
+    MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+    Console.WriteLine($"Произведение первой и второй матриц:");
+    writearr(resultMatrix);
+
+    void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+    {
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < firstMartrix.GetLength(1); k++)
+                {
+                    sum += firstMartrix[i, k] * secomdMartrix[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+    }
+
+
+
+    void CreateArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(range);
+            }
+        }
+    }
+
+    void writearr(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("|");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.Write("|");
+            Console.WriteLine();
+
+        }
+    }
+
+}
---
+++ lesoon 8 part 2/zadanie 62/zadanie 62/Program.cs
@@ -0,0 +1,38 @@
+﻿int n = 4;
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+    sqareMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+writearr(sqareMatrix);
+
+void writearr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine();
+
+    }
+}
+
