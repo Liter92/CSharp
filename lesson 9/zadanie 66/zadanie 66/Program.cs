973d45cf3a374577abf3392d7d8dbedcf705fea6
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-06-19 18:49:56 +0300

    Add files via upload

---
+++ lesson 9/zadanie 66/zadanie 66/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число m:");
+int numbrm = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите коненчное число n:");
+int numbrn = int.Parse(Console.ReadLine());
+if (numbrm<0 || numbrn<0) 
+    Console.WriteLine("введите положительные значения");
+
+else
+{ 
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void sumelem(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (numberM++);
+    sumelem(numberM, numberN, sum);
+}
+
+sumelem(numbrm, numbrn, 0);
+   
+}
---
+++ lesson 9/zadanie 68/zadanie 68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+if (n < 0 || m < 0)
+    Console.WriteLine("введите положительные значения");
+else
+{
+    int functionAkkerman = Ack(m, n);
+
+    Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+    int Ack(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if (n == 0) return Ack(m - 1, 1);
+        else return Ack(m - 1, Ack(m, n - 1));
+    }
+
+}
